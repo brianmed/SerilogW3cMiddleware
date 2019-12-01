419d475d8682ee565b7d25dbacdc1656736f977d
Brian Medley <pub-github@bmedley.org>
2019-12-01 13:36:39 -0600

    Changes per IRC code review

--- SerilogW3cMiddleware.cs
+++ SerilogW3cMiddleware.cs
@@ -20,10 +20,26 @@ namespace BrianMed.AspNetCore.SerilogW3cMiddleware
         public bool DisplayExceptions { get; set; } = true;
 
         public bool RethrowExceptions { get; set; } = true;
+
+        public bool StrictW3C { get; set; } = false;
     }
 
     public class SerilogW3cMiddleware
     {
+        private class LogProperties
+        {
+            public string RemoteIpAddress { get; set; } = String.Empty;
+            public string AuthUser { get; set; } = String.Empty;
+            public string Date { get; set; } = String.Empty;
+            public string Method { get; set; } = String.Empty;
+            public string Path { get; set; } = String.Empty;
+            public string Protocol { get; set; } = String.Empty;
+            public string StatusCode { get; set; } = String.Empty;
+            public string ContentLength { get; set; } = String.Empty;
+            public string ElapsedMs { get; set; } = String.Empty;
+            public string Identifier { get; set; } = String.Empty;
+        }
+
         private readonly RequestDelegate Next;
         private readonly SerilogW3cMiddlewareOptions Options;
 
@@ -36,43 +52,83 @@ namespace BrianMed.AspNetCore.SerilogW3cMiddleware
 
         public async Task Invoke(HttpContext context)
         {
-                long start = Stopwatch.GetTimestamp();
+            if (Options.StrictW3C && Options.DisplayBefore) {
+                throw new ArgumentException("Can not set both StrictW3C and DisplayBefore");
+            }
 
-                // https://en.wikipedia.org/wiki/Common_Log_Format
-                try {
+            long start = Stopwatch.GetTimestamp();
+
+            LogProperties logProperties = new LogProperties();
+
+            // https://en.wikipedia.org/wiki/Common_Log_Format
+            try {
+                DateTime now = DateTime.Now;
+
+                logProperties.RemoteIpAddress = context.Request.HttpContext.Connection.RemoteIpAddress.ToString();
+                logProperties.AuthUser = context.User.Identity.Name ?? "-";
+                logProperties.Date = $"{now.ToString("dd/MMM/yyyy:HH:MM:ss ")}{now.ToString("zzz").Replace(":", "")}";
+                logProperties.Method = context.Request.Method;
+                logProperties.Path = context.Request.Path;
+                logProperties.Protocol = context.Request.Protocol;
+                logProperties.StatusCode = "000";
+                logProperties.ContentLength = "0";
+                logProperties.ElapsedMs = "0";
+                logProperties.Identifier = $"begin:{context.TraceIdentifier}";
+
+                if (Options.DisplayBefore) {
+                    LogIt(logProperties);
+                }
+
+                await Next.Invoke(context);
+
+                if (Options.DisplayAfter) {
+                    now = DateTime.Now;
+
+                    logProperties.Date = $"{now.ToString("dd/MMM/yyyy:HH:MM:ss ")}{now.ToString("zzz").Replace(":", "")}";
+                    logProperties.StatusCode = context.Response.StatusCode.ToString();
+                    logProperties.ContentLength = $"{context.Response?.ContentLength ?? -1}";
+                    logProperties.ElapsedMs = GetElapsedMilliseconds(start, Stopwatch.GetTimestamp()).ToString();
+                    logProperties.Identifier = $"end:{context.TraceIdentifier}";
+
+                    LogIt(logProperties);
+                }
+            } catch (Exception ex) {
+                if (Options.DisplayAfter) {
                     DateTime now = DateTime.Now;
 
-                    if (Options.DisplayBefore) {
-                        Log.Information($"{context.Request.HttpContext.Connection.RemoteIpAddress} - {context.User.Identity.Name ?? "-"} {now.ToString("dd/MMM/yyyy:HH:MM:ss ")}{now.ToString("zzz").Replace(":", "")} \"{context.Request.Method} {context.Request.Path} {context.Request.Protocol}\" begin:{context.TraceIdentifier}");
-                    }
+                    logProperties.Date = $"{now.ToString("dd/MMM/yyyy:HH:MM:ss ")}{now.ToString("zzz").Replace(":", "")}";
+                    logProperties.StatusCode = "500";
+                    logProperties.ContentLength = $"{context.Response?.ContentLength ?? -1}";
+                    logProperties.ElapsedMs = GetElapsedMilliseconds(start, Stopwatch.GetTimestamp()).ToString();
+                    logProperties.Identifier = $"end:{context.TraceIdentifier}";
 
-                    await Next.Invoke(context);
-                    double elapsedMs = GetElapsedMilliseconds(start, Stopwatch.GetTimestamp());
-
-                    if (Options.DisplayAfter) {
-                        now = DateTime.Now;
-                        Log.Information($"{context.Request.HttpContext.Connection.RemoteIpAddress} - {context.User.Identity.Name ?? "-"} {now.ToString("dd/MMM/yyyy:HH:MM:ss ")}{now.ToString("zzz").Replace(":", "")} \"{context.Request.Method} {context.Request.Path} {context.Request.Protocol}\" {context.Response.StatusCode} {context.Response?.ContentLength ?? -1} {elapsedMs} end:{context.TraceIdentifier}");
-                    }
-                } catch (Exception ex) {
-                    double elapsedMs = GetElapsedMilliseconds(start, Stopwatch.GetTimestamp());
-
-                    if (Options.DisplayAfter) {
-                        DateTime now = DateTime.Now;
-                        Log.Information($"{context.Request.HttpContext.Connection.RemoteIpAddress} - {context.User.Identity.Name ?? "-"} {now.ToString("dd/MMM/yyyy:HH:MM:ss ")}{now.ToString("zzz").Replace(":", "")} \"{context.Request.Method} {context.Request.Path} {context.Request.Protocol}\" 500 {context.Response?.ContentLength ?? -1} {elapsedMs} end:{context.TraceIdentifier} {ex.Message.Replace("\n", "|")}");
-                    }
-
-                    if (Options.DisplayExceptions) {
-                        Log.Debug(ex.ToString());
-                    }
-
-                    if (Options.RethrowExceptions) {
-                        throw;
-                    }
+                    LogIt(logProperties);
                 }
+
+                if (Options.DisplayExceptions) {
+                    Log.Error(ex, $"Exception during \"{logProperties.Method} {logProperties.Path} {logProperties.Protocol}\"");
+                }
+
+                if (Options.RethrowExceptions) {
+                    throw;
+                }
+            }
+        }
+
+        private void LogIt(LogProperties properties)
+        {
+            string messageTemplateDefault = "{RemoteIpAddress} - {AuthUser} [{Date}] \"{Method} {Path} {Protocol}\" {StatusCode} {ContentLength} {ElapsedMs} {Identifier}";
+            string messageTemplateStrictW3c = "{RemoteIpAddress} - {AuthUser} [{Date}] \"{Method} {Path} {Protocol}\" {StatusCode} {ContentLength}";
+
+            if (Options.StrictW3C) {
+                Log.Information(messageTemplateStrictW3c, properties.RemoteIpAddress, properties.AuthUser, properties.Date, properties.Method, properties.Path, properties.Protocol, properties.StatusCode, properties.ContentLength);
+            } else {
+                Log.Information(messageTemplateDefault, properties.RemoteIpAddress, properties.AuthUser, properties.Date, properties.Method, properties.Path, properties.Protocol, properties.StatusCode, properties.ContentLength, properties.ElapsedMs, properties.Identifier);
+            }
         }
 
         // Serilog
-        static double GetElapsedMilliseconds(long start, long stop)
+        public double GetElapsedMilliseconds(long start, long stop)
         {
             return (stop - start) * 1000 / (double)Stopwatch.Frequency;
         }
